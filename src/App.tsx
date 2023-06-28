50d7265808d391c604373907978314f662a704cf
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2023-06-28 14:37:17 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import profile from './components/profile.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className='flex padding-header place-content-between align-items-center'>
+
+          <a href=""><img className='rounded' src={profile}></img></a>
+          <a href="">Projetos</a>
+          teste
+
+        </header>
     </div>
   );
 }
