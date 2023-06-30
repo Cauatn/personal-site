e56f2b782ad1952c5b0ca4f9d4597eed7e2c3104
Cauã Tavares <39890456+CauanGx@users.noreply.github.com>
2023-06-30 15:52:29 -0300

    semi-fishing main page

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,10 +3,10 @@ import Style from './H1.module.css'
 export default function H1() {
     return (
         <div className={Style.Main}>
-            <h1 className={Style.h1}> Oi, eu sou cauã tavares </h1>
+            <h1 className={Style.h1}> oi, sou cauã tavares </h1>
             
             <p className = {Style.text}>
-                Desenvolvedor React e React Native interessado em assuntos Web
+                Desenvolvedor React e React Native interessado em qualquer assunto relacionado a desenvolvimento Web.
             </p>
 
             <ul >
@@ -15,6 +15,10 @@ export default function H1() {
 	            </svg>
                 </li>
             </ul>
+
+            <h2 className={Style.h2}>Sobre mim</h2>
+            <p>Atualmente sou estudante de Engenharia de Computação pela Universidade Federal do Vale do São Francisco UNIVASF, novo no mundo de desenvolvimento, procurando sempre aprender novas tecnologias e fundamentar melhor meus conhecimentos.</p>
+            <p>Sinta-se livre para entrar em contato comigo através de minhas redes!</p>
         </div>
     )
 }
